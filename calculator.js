400bda68b49a4cbc98918e07ace5c6c5714a3955
GuilhermeVG <vg.guilherme@gmail.com>
2025-09-16 10:17:02 -0300

    Adicionado return em cada função

--- calculator.js
+++ calculator.js
@@ -1,16 +1,23 @@
 // retorna o valor da soma entre os dois parâmetros
-function sum(v1, v2) {}
+function sum(v1, v2) {
+  return v1 + v2;
+}
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
+function sub(v1, v2) {
+  return v1 - v2;
+}
 
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
+function div(v1, v2) {
+  // Retorna uma mensagem de erro se a divisão for por zero
+  if (v2 === 0) {
+    return "Erro: Divisão por zero não é permitida.";
+  }
+  return v1 / v2;
+}
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
-
-// retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
-
-console.log(sum(v1, v2));
+function mult(v1, v2) {
+  return v1 * v2;
+}
